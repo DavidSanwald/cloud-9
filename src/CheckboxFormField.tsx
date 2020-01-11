6614ea8cb39d5e686a1d55c7f17c23a50301855c
DavidSanwald <david.sanwald@gmail.com>
2020-01-11 01:20:42 +0100

    update field abstractions

--- src/CheckboxFormField.tsx
+++ src/CheckboxFormField.tsx
@@ -1,17 +1,21 @@
 import { FieldProps, getIn } from "formik";
 import React from "react";
-import { Checkbox } from "@material-ui/core";
+import { Checkbox, FormControlLabel, CheckboxProps } from "@material-ui/core";
 
-export const CheckboxFormField: React.FC<FieldProps> = ({
+export const CheckboxFormField: React.FC<FieldProps & { label: string }> = ({
   field,
   form,
+  label,
   ...props
 }) => {
-
+  console.log(field);
+  console.log(form);
   return (
-    <Checkbox
+    <FormControlLabel
       {...field}
       {...props}
+      label={label}
+      control={<Checkbox value="upfront" checked={field.value.upfront} />}
     />
   );
 };
