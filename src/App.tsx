05f572f9ab18e7fdc8a3a4ea41644a3eb681f2fb
DavidSanwald <david.sanwald@gmail.com>
2020-01-12 13:06:33 +0100

    factor out app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import { CloudForm } from "CloudForm";
 import { postData } from "api";
+import { makeStyles, AppBar, Toolbar, Typography } from "@material-ui/core";
 
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    position: "relative"
+  }
+}));
 const App: React.FC = () => {
-  return <CloudForm sendData={postData} />;
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="absolute" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Cloud 9
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <CloudForm sendData={postData} />;
+    </>
+  );
 };
 
 export default App;
